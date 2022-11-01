635b4e8b0a2749bf83cd80ecf8c894dabf569716
Mono Kung <mono@email.com>
2022-11-01 12:36:32 +0800

    Install IIS

--- DNS/install-dns.ps1
+++ DNS/install-dns.ps1
@@ -1,3 +1,5 @@
 $cred=Get-Credential
 Enter-PSSession -VMName Server3 -Credential $cred
 Install-WindowsFeature dns -includemanagementtools
+Add-DnsServerPrimaryZone -Name "fujistore.com.hk" -ZoneFile "fujistore.com.hk.dns"
+Install-WindowsFeature Web-Server -IncludeManagementTools
