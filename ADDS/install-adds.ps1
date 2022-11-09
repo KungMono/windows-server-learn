ca0ffafff5f6e1334611174e76c967c088329713
Mono Kung <mono@email.com>
2022-11-09 17:40:26 +0800

    Install-ADDS

---
+++ ADDS/install-adds.ps1
@@ -0,0 +1,24 @@
+
+
+Rename-Computer -Newname "Server4" -Restart
+
+
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-Credential (Get-Credential) `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainName "fujifuji.com" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
