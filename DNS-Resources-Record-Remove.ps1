e048e1219ef82563c54c9d780b9fe9903f31b4a8
KungMono <115074455+KungMono@users.noreply.github.com>
2022-11-14 18:19:29 +0800

    Update DNS-Resources-Record-Remove.ps1

--- DNS-Resources-Record-Remove.ps1
+++ DNS-Resources-Record-Remove.ps1
@@ -14,3 +14,13 @@ Get-DnsServerResourceRecord -computername 10.166.38.4 -ZoneName hk.fpca -rrtype 
 Get-DnsServerResourceRecord -computername 10.166.38.4 -ZoneName hk.fpca -rrtype a | select hostname, recordtype, timestamp -first 30 -expand recorddata | export-csv "dns.csv" -NoTypeInformation
 ii dns.csv
 gc .\dns.csv | select -First 3
+
+$ComputerName = "10.166.38.4"
+$ZoneName = "hk.contoso.com"
+$RRType = "A"
+$Servers = Import-Csv -Path dns2.csv
+foreach ($Server in $Servers) {
+    Write-Host "Removing DNS record for $($Server.Hostname)" -ForegroundColor red -BackgroundColor white
+    Remove-DnsServerResourceRecord -ComputerName $ComputerName -ZoneName $ZoneName -RRType $RRType -Name $Server.HostName -Whatif
+    Start-Sleep -Seconds 0.5
+}
